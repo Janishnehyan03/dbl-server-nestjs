adb4bcae936683a26fa785843ccd474a5d3eb1e7
Janishnehyan03 <nehyanjanish@gmail.com>
2025-04-17 10:00:58 +0530

    feat: update CORS configuration to allow requests from the production client

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   app.use(morgan('dev'));
 
   app.enableCors({
-    origin: 'http://localhost:5001', // Make sure this matches your frontend
+    origin: ['http://localhost:5001', 'https://dbl-client.vercel.app'], // Make sure this matches your frontend
     methods: 'GET, POST, PUT, DELETE, OPTIONS, PATCH',
     allowedHeaders: 'Content-Type, Authorization',
     credentials: true, // ✅ Required for sending cookies/auth headers
