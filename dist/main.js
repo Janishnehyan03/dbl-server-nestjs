0266c6c261f8ed5ae58fa7dae627add5e7766ea7
Janishnehyan03 <nehyanjanish@gmail.com>
2025-04-17 10:02:41 +0530

    Refactor code structure for improved readability and maintainability

--- dist/main.js
+++ dist/main.js
@@ -7,7 +7,7 @@ async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     app.use(morgan('dev'));
     app.enableCors({
-        origin: 'http://localhost:5001',
+        origin: ['http://localhost:5001', 'https://dbl-client.vercel.app'],
         methods: 'GET, POST, PUT, DELETE, OPTIONS, PATCH',
         allowedHeaders: 'Content-Type, Authorization',
         credentials: true,
